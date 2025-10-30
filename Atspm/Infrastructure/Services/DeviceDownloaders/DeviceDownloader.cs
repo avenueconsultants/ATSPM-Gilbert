fac31fe7f73f744da6a65b6aa8847d6d412c01b4
Jaithra <jbhatia@avenueconsultants.com>
2025-10-30 10:01:10 -0600

    Leave current fixes

--- Atspm/Infrastructure/Services/DeviceDownloaders/DeviceDownloader.cs
+++ Atspm/Infrastructure/Services/DeviceDownloaders/DeviceDownloader.cs
@@ -59,17 +59,24 @@ namespace Utah.Udot.Atspm.Infrastructure.Services.DeviceDownloaders
         /// <returns></returns>
         public virtual Uri GenerateLocalFilePath(Device device, Uri resource)
         {
-            var fileExtension = Path.HasExtension(resource.Segments.LastOrDefault()) ? Path.GetExtension(resource.Segments.LastOrDefault()) : ".txt";
-            var deviceIdentifier = device.DeviceIdentifier ?? "999999";
+            var fileName = Path.GetFileName(resource.LocalPath);
+            if (!Path.HasExtension(fileName))
+            {
+                fileName += ".txt";
+            }
 
-            var fileName = $"{deviceIdentifier}-{DateTime.Now.Ticks}{fileExtension}";
-            var path = Path.Combine
-                (_options.BasePath,
-                $"{device.Location?.LocationIdentifier}",
+            var deviceIdentifier = string.IsNullOrEmpty(device.DeviceIdentifier)
+                ? "999999"
+                : device.DeviceIdentifier;
+
+            var path = Path.Combine(
+                _options.BasePath,
+                device.Location?.LocationIdentifier ?? string.Empty,
                 device.DeviceType.ToString(),
-                 $"{deviceIdentifier}-{device.Ipaddress}");
+                $"{deviceIdentifier}-{device.Ipaddress}"
+            );
 
-            var result = new UriBuilder()
+            var result = new UriBuilder
             {
                 Scheme = Uri.UriSchemeFile,
                 Path = Path.Combine(path, fileName)
@@ -295,9 +302,24 @@ namespace Utah.Udot.Atspm.Infrastructure.Services.DeviceDownloaders
 
             foreach (var i in _value.Split('[', ']').ToArray())
             {
-                if (i.StartsWith("DateTime"))
+                if (i.StartsWith("EndDateTime"))
                 {
-                    builder.AppendFormat("{0" + i.Replace("DateTime", "") + "}", DateTime.Now);
+                    if (_obj is Device d && d.DeviceConfiguration != null)
+                    {
+                        DateTime nowToMinute = DateTime.Now
+                            .AddSeconds(-DateTime.Now.Second)
+                            .AddMilliseconds(-DateTime.Now.Millisecond);
+                        builder.AppendFormat("{0" + i.Replace("EndDateTime", "") + "}", nowToMinute.AddMinutes(-d.DeviceConfiguration.LoggingOffset));
+                    }
+                }
+
+                else if (i.StartsWith("DateTime"))
+                {
+                    DateTime nowToMinute = DateTime.Now
+                    .AddSeconds(-DateTime.Now.Second)
+                    .AddMilliseconds(-DateTime.Now.Millisecond);
+
+                    builder.AppendFormat("{0" + i.Replace("DateTime", "") + "}", nowToMinute.AddMinutes(-30));
                 }
 
                 else if (i.StartsWith("LogStartTime"))
