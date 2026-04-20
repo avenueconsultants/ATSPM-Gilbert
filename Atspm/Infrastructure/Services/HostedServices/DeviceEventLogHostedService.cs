e00a7595db69dc3d8ef813063cc9eee344143062
kford <kford@avenueconsultants.com>
2026-04-20 13:26:34 -0600

    undoing an un-nessisary change.

--- Atspm/Infrastructure/Services/HostedServices/DeviceEventLogHostedService.cs
+++ Atspm/Infrastructure/Services/HostedServices/DeviceEventLogHostedService.cs
@@ -17,7 +17,6 @@
 
 using Lextm.SharpSnmpLib.Messaging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Diagnostics;
@@ -39,8 +38,6 @@ namespace Utah.Udot.Atspm.Infrastructure.Services.HostedServices
     public class DeviceEventLogHostedService(ILogger<DeviceEventLogHostedService> log, IServiceScopeFactory serviceProvider, IOptions<DeviceEventLoggingConfiguration> options) : HostedServiceBase(log, serviceProvider)
     {
         private readonly IOptions<DeviceEventLoggingConfiguration> _options = options;
-        
-
 
         /// <inheritdoc/>
         public override async Task Process(IServiceScope scope, Stopwatch stopwatch, CancellationToken cancellationToken = default)
